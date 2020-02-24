d651f8a243a540b75eac6bca0e63c688b2f9d1cc
Sammy H <pokeso123@hotmail.com>
2020-02-23 17:15:10 -0800

    added oculus support, camera & hand tracking, made map

---
+++ Assets/Scripts/HandMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    public Transform rightHand;
+    public Transform leftHand;
+    void Update()
+    {
+        rightHand.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        leftHand.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+
+        rightHand.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+        leftHand.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+
+    }
+}
